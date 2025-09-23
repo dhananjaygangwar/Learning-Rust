44e85022339d59e8ce0830f1e289aaf5d95ebbfb
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-09-23 09:13:55 +0530

    child modules

--- app/src/helpers.rs
+++ app/src/helpers.rs
@@ -1,4 +1,6 @@
-pub fn get_full_name(first: &str, last: &str) -> String {
-    let full_name: String = format!("{0} {1}", first, last);
-    return full_name;
+pub mod namehelpers {
+    pub fn get_full_name(first: &str, last: &str) -> String {
+        let full_name: String = format!("{0} {1}", first, last);
+        return full_name;
+    }
 }
